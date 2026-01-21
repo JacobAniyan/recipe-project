3390883a50637c4af0d4acee4a5111dd911d77bb
Jared <jared@Jareds-MacBook-Pro.local>
2026-01-21 20:33:28 +0000

    Results Page

--- src/components/IndividualRecipePage.jsx
+++ src/components/IndividualRecipePage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Loading from "./Loading";
 
-function ReceipeDetailPage() {
+function IndividualRecipePage() {
   const { id } = useParams();
 
   const [recipe, setRecipe] = useState(null);
