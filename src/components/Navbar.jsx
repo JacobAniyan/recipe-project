521512764e05fbc6ba0947509c3e518b65a4c5e9
AK Naresh <ak@AKs-MacBook-Pro-2.local>
2026-01-27 10:53:35 +0000

    refactored code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/Logo(RecipeGen).png";
+import logo from "../assets/Logo.png";
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/" className="logo-link">
+      <Link to="/" className="logo-link" aria-label="Homepage/ Search Recipe">
         <img src={logo} alt="Recipe Gen Logo" className="brand-logo" />
       </Link>
       <div className="nav-links">
